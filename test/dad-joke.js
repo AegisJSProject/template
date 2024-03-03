5b2338428eb30aa2d34cb9ff10dd43c2ae5e0408
Chris Zuber <shgysk8zer0@gmail.com>
2024-03-03 15:34:49 -0800

    Update dependencies

--- test/dad-joke.js
+++ test/dad-joke.js
@@ -1,64 +1,34 @@
-import { html, css, replace, appendTo, addStyles } from '@aegisjsproject/core';
-import { gray } from '@aegisjsproject/styles/palette/bootstrap.js';
-import { AegisComponent, SYMBOLS, TRIGGERS } from '@aegisjsproject/component';
+import { html } from '@aegisjsproject/core/parsers/html.js';
+import { replace } from '@aegisjsproject/core/dom.js';
+import { AegisComponent } from '@aegisjsproject/component/base.js';
+import { TRIGGERS, SYMBOLS } from '@aegisjsproject/component/consts.js';
 import { updateIcon } from './icons.js';
+import { styles } from './dad-joke-styles.js';
+import { template } from './dad-joke-html.js';
 
 class HTMLDataJokeElement extends AegisComponent {
-	async [SYMBOLS.render](type, { shadow }) {
+	constructor() {
+		super({ template, styles, exportParts: { joke: 'dad-joke', btn: 'refresh-joke-btn' }});
+	}
+
+	async [SYMBOLS.render](type, { shadow, signal, ...data }) {
 		switch(type) {
 			case TRIGGERS.constructed:
-				addStyles(shadow, css`:host {
-					padding: 1.2rem;
-					width: clamp(400px, 100%, 600px);
-					border-radius: 14px;
-					border: 1px solid ${gray[3]};
-				}
-
-				[part="joke"] {
-					height: 4rem;
-					overflow-y: auto;
-				}
-
-				::slotted(p) {
-					margin: 0;
-				}
-
-				.icon {
-					vertical-align: bottom;
-					height: 1em;
-					height: 1lh;
-					width: auto;
-				}
-
-				.joke {
-					font-family: system-ui;
-					padding: 1.3rem;
-					border-radius: 8px;
-					border: 1px solid #cacaca;
-				}`);
-
-				appendTo(shadow, html`<div part="container">
-					<div part="joke">
-						<slot name="joke">Loading...</slot>
-					</div>
-					<button type="button" id="update-btn" class="btn btn-primary" part="btn">
-						<span>Get new Dad Joke</span>
-					</button>
-				</div>`);
-
-				// Cannot add listeners or `on*` attributes using `html`
-				shadow.getElementById('update-btn').addEventListener('click', () => this.triggerUpdate('click'));
-
 				// Also, for now, elements of a different namespace, such as `<svg>` are not supported
 				shadow.getElementById('update-btn').append(updateIcon.cloneNode(true));
-
 				break;
 
 			case TRIGGERS.connected:
-			case 'click':
-				replace(this, html`<p slot="joke">${await HTMLDataJokeElement.getJoke()}</p>`);
+				this.update({ signal });
 				break;
+
+			default:
+				console.log({ type, ...data });
 		}
+	}
+
+	async update({ signal } = {}) {
+		replace(this, html`<p slot="joke">${await HTMLDataJokeElement.getJoke({ signal })}</p>`);
 	}
 
 	static async getJoke({ signal } = {}) {

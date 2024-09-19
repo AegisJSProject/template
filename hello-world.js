6c2890ea76726c1485f0e11639050614fd45af72
Chris Zuber <shgysk8zer0@gmail.com>
2024-09-19 10:07:17 -0700

    Update dependencies & config

--- hello-world.js
+++ hello-world.js
@@ -1,10 +1,20 @@
 import { AegisComponent } from '@aegisjsproject/component/base.js';
+import { SYMBOLS, TRIGGERS } from '@aegisjsproject/component/consts.js';
 
 class HTMLHelloWorldElement extends AegisComponent {
 	constructor() {
 		super({
-			template: '<h1><slot name="content">Hello, World!</slot></h1>',
+			template: '<h1><slot name="content">Loading...</slot></h1>',
 		});
+	}
+
+	async [SYMBOLS.render](type) {
+		if (type === TRIGGERS.connected) {
+			const el = document.createElement('span');
+			el.textContent = 'Hello, World!';
+			el.slot = 'content';
+			this.append(el);
+		}
 	}
 }
 
